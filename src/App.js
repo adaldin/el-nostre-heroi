677df3249a3b9ff923074654df79838fcea68477
Hiena <aledaldin@gmail.com>
2022-05-11 15:54:49 +0200

    Base de proyecto (a modificar)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import data from "./data.js";
+import Escena from "./components/escena/Escena";
+import Buttons from "./components/buttons/Buttons";
+import Welcome from "./components/escena/Welcome";
 
 function App() {
+  const [position, setPosition] = useState(0);
+  const [isShown, setIsShown] = useState(true);
+  const phrasesElements = data.map((p) => {
+    return (
+      <Escena
+        key={data.indexOf(p)}
+        phrase={p.text}
+        id={data.indexOf(p)}
+        currentPosition={position}
+        img={p.img}
+      ></Escena>
+    );
+  });
+
+  function handleClick(e) {
+    if (e.target.name === "Següent") {
+      setPosition((prevPosition) => {
+        return prevPosition === data.length - 1 ? 0 : prevPosition + 1;
+      });
+    } else if (e.target.name === "Anterior") {
+      setPosition((prevPosition) => {
+        return prevPosition === 0 ? data.length - 1 : prevPosition - 1;
+      });
+    }
+  }
+  function handleBeggining() {
+    setIsShown((prevShown) => !prevShown);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {isShown && <Welcome handleBegining={handleBeggining} />}
+      {!isShown && (
+        <div>
+          <Buttons handleClick={handleClick} name="Anterior"></Buttons>
+          <Buttons handleClick={handleClick} name="Següent"></Buttons>
+          {phrasesElements}
+        </div>
+      )}
+    </React.Fragment>
   );
 }
 
 export default App;
+
+// return (
+//   <React.Fragment>
+//     {isShown && <Welcome handleBegining={handleBeggining} />}
+//     {!isShown && (
+//       <div>
+//         <Buttons handleClick={handleClick} name="Anterior"></Buttons>
+//         <Buttons handleClick={handleClick} name="Següent"></Buttons>
+//         {phrasesElements}
+//       </div>
+//     )}
+//   </React.Fragment>
+// );
